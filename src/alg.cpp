b3b726485442efbecbfb69211f8f89d8886a90b8
Артём <71376415+artemik2ww@users.noreply.github.com>
2026-05-03 17:13:07 +0300

    b125
    b125

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,64 @@
 // Copyright 2021 NNTU-CS
+
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+
+template <class T>
+class TPQueue {
+ private:
+  struct Node {
+    T data;
+    Node* next;
+
+    explicit Node(const T& value) : data(value), next(nullptr) {}
+  };
+
+  Node* head;
+
+ public:
+  TPQueue() : head(nullptr) {}
+
+  ~TPQueue() {
+    while (head != nullptr) {
+      Node* tmp = head;
+      head = head->next;
+      delete tmp;
+    }
+  }
+
+  bool isEmpty() const {
+    return head == nullptr;
+  }
+
+  void push(const T& value) {
+    Node* node = new Node(value);
+
+    if (head == nullptr || value.prior > head->data.prior) {
+      node->next = head;
+      head = node;
+      return;
+    }
+
+    Node* current = head;
+
+    while (current->next != nullptr &&
+           current->next->data.prior >= value.prior) {
+      current = current->next;
+    }
+
+    node->next = current->next;
+    current->next = node;
+  }
+
+  T pop() {
+    T result = head->data;
+
+    Node* tmp = head;
+    head = head->next;
+    delete tmp;
+
+    return result;
+  }
+};
+
+#endif  // INCLUDE_TPQUEUE_H_
